949e4ee3dce474dd74a6ddc0f9211e10ccf390ef
Akky01 <138700227+AkShAypatil119@users.noreply.github.com>
2024-05-04 12:53:26 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -9,10 +9,10 @@ def hello():
   Returns:
      A string with the words 'hello word!'.
 
-     """"
+     """
   return "hello word!"
 
-if __name__ =="__main__":
+if __name__ =="__cc__":
 
   app.run(host="127.0.0.1",port=8080,debug=True)
      
