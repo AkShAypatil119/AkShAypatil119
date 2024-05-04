642bd81283bc759c737bf7b08222fcb3b3c07a4b
Akky01 <138700227+AkShAypatil119@users.noreply.github.com>
2024-05-04 12:49:56 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -1,1 +1,18 @@
-print('hello world ')
+from flask import flask
+
+app=Flask(_name_)
+
+@app.route("/")
+def hello():
+  """Return a friendly HTTP greeting.
+
+  Returns:
+     A string with the words 'hello word!'.
+
+     """"
+  return "hello word!"
+
+if __name__ =="__main__":
+
+  app.run(host="127.0.0.1",port=8080,debug=True)
+     
